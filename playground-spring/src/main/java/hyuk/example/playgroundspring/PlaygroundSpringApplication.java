8bd5dc2e6897edb9d2835eab181e4bf2ad4bfff1
eunhyuk-bahng <wv00075@wv-works.com>
2024-03-01 15:54:27 +0900

    chore: init project

---
+++ playground-spring/src/main/java/hyuk/example/playgroundspring/PlaygroundSpringApplication.java
@@ -0,0 +1,13 @@
+package hyuk.example.playgroundspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaygroundSpringApplication.class, args);
+    }
+
+}
---
+++ playground-spring/src/test/java/hyuk/example/playgroundspring/PlaygroundSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hyuk.example.playgroundspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
